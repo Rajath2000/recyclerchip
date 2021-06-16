86bd6fa4d9d41c2aac122a1ab2fb7eae9eefae91
Rajath2000 <rajathmr2000@gmai.com>
2021-06-16 17:32:24 +0530

    hello

--- app/src/main/java/com/example/recyclerchip/MainActivity.java
+++ app/src/main/java/com/example/recyclerchip/MainActivity.java
@@ -6,16 +6,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Adapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView rcv;
+//    RecyclerView rcv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rcv=(RecyclerView) findViewById(R.id.relview);
-        rcv.setLayoutManager(new LinearLayoutManager(this));
-        String arr[]={"cat","rat","bat","areca"};
-        rcv.setAdapter(new myadapter(arr));
+//        rcv=(RecyclerView) findViewById(R.id.relview);
+//        rcv.setLayoutManager(new LinearLayoutManager(this));
+//        String arr[]={"cat","rat","bat","areca"};
+//        rcv.setAdapter(new myadapter(arr));
+
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.maincontainer,new DataFragment()).commit();
+
+
     }
 }
--- app/src/main/java/com/example/recyclerchip/myadapter.java
+++ app/src/main/java/com/example/recyclerchip/myadapter.java
@@ -1,24 +1,34 @@
 package com.example.recyclerchip;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.chip.Chip;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
 public class myadapter extends RecyclerView.Adapter<myadapter.holder>
 {
+  
+    ArrayList<String> favratious=new ArrayList<>();
+    public ArrayList<String> Userselected=new ArrayList<>();
 
-    String data[];
 
-    public myadapter(String[] data)
-    {
-        this.data = data;
+    public myadapter(ArrayList<String> favratious) {
+        this.favratious = favratious;
     }
 
+    public ArrayList<String> UserSelectedDetails()
+    {
+        return Userselected;
+    }
     @NonNull
     @Override
     public holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -29,21 +39,50 @@ public class myadapter extends RecyclerView.Adapter<myadapter.holder>
 
     @Override
     public void onBindViewHolder(@NonNull holder holder, int position) {
-       holder.chip.setText(data[position]);
+       holder.chip.setText(favratious.get(position));
+       Userselected.add("");
+
+       holder.chip.setOnClickListener(new View.OnClickListener() {
+           private static final String TAG = "adapter";
+
+           @Override
+           public void onClick(View v) {
+               if(holder.chip.isChecked())
+               {
+                   Log.d(TAG, "onClick: checked");
+                   Userselected.set(position,favratious.get(position));
+
+               }
+               else
+               {
+                   Userselected.set(position,"");
+                   Log.d(TAG, "onClick: not checked");
+
+               }
+
+               for(String arrayList:Userselected)
+               {
+                   Log.d(TAG, "onClick: "+arrayList);
+               }
+           }
+       });
+
 
     }
 
     @Override
     public int getItemCount() {
-        return data.length;
+        return favratious.size();
     }
 
 
     class holder extends RecyclerView.ViewHolder {
         Chip chip;
+        FloatingActionButton floatbutton;
         public holder(@NonNull View itemView) {
             super(itemView);
             chip=(Chip)itemView.findViewById(R.id.chip4);
+
         }
     }
 }
